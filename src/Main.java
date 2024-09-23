9691226c8aaf95b77cdc277a98935273b7ed758b
lunaviej <lunaviej@mail.uc.edu>
2024-09-23 03:35:44 -0400

    Completed Task $

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+       int intModulo = 0;
+       int numToExamine = -8;
+       intModulo = numToExamine;
+        System.out.println("The number is not even " + intModulo);
+
     }
 }
